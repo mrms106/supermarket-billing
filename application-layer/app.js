ce263459640348c65f0683bbdf7ffcbf675985e9
Makardhwaj <mhs93679@gmail.com>
2024-11-14 14:39:38 +0530

    connected to the mongo database

--- application-layer/app.js
+++ application-layer/app.js
@@ -1,5 +1,16 @@
 const express= require("express")
-const app= express()
+const mongoose=require("mongoose")
+const cors=require("cors")
+const app = express()
+
+// database connection
+main().catch(err => console.log(err));
+
+async function main() {
+ const databaseUrl='mongodb://127.0.0.1:27017/supermarket'
+  await mongoose.connect(databaseUrl);
+  console.log("The Database is Connected....")
+}
 
 app.get("/",(req,res)=>{
     res.send("working backend")
