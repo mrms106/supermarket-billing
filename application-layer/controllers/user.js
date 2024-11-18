3fe4a037764d290dd132adb4c4800adb13503330
Makardhwaj <mhs93679@gmail.com>
2024-11-18 15:44:05 +0530

    added login and logout

--- application-layer/controllers/user.js
+++ application-layer/controllers/user.js
@@ -18,3 +18,17 @@ module.exports.signup=async(req,res)=>{
         res.status(400).json({message:"something went wrong"})
     }
   }
+
+  module.exports.login=(req,res)=>{
+        res.status(200).json({ message: 'Login successful', user: req.user });
+  }
+
+  module.exports.logout=async(req,res)=>{
+    req.logout((err)=>{
+        if(err){
+            res.status(500).json({ message: 'failed to logout', error: err.message });
+            return next(err)
+        }
+        res.status(200).json({message:"logout successfull"})
+    })
+  }
--- application-layer/routes/user.js
+++ application-layer/routes/user.js
@@ -1,8 +1,11 @@
 const express=require('express')
 const router=express.Router()
 const usercontroller=require('../controllers/user')
+const passport = require('passport')
 
 router.post('/signup',usercontroller.signup)
+router.post('/login',passport.authenticate('local'),usercontroller.login)
+router.get('/logout',usercontroller.logout)
 
 
 
